8924838ba13271d2ce945fe2f77726218d47ca74
Rohan85041 <chrohankumar85041@gmail.com>
2026-05-29 15:54:45 +0530

    new changes

--- src/header.jsx
+++ src/header.jsx
@@ -1,13 +1,24 @@
-import {useState} from "react"
+import { useState } from "react"
 function Header() {
-  const [count, setcount] = useState(0)
+ const [userinput, setuserinput] = useState('')
 
-  function handlecount(){
-    setcount(count+1)
-  }
   return (
     <>
-    <button onClick={handlecount} >count {count}</button>
+   <header style={{display:"flex", justifyContent:"space-around"}}  >
+    <img src="src/assets/vite.svg" alt="LOGO" />
+
+    <input onChange={(e)=>setuserinput(e.target.value)}   style={{borderRadius:"10px"}}     type="text" />
+
+     
+    <nav style={{display:"flex",gap:"10px" }} >
+        <a href="">HOME</a>
+        <a href="">PRODUCTS</a>
+        <a href="">ABOUT</a>
+        <a href="">SIGNUP</a>
+        <a href="">SIGNIN</a>
+    </nav>
+    {userinput}
+   </header>
     </>
   )
 }
